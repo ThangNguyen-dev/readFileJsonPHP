7c3621ece22e94d6ed0905197801b1f493c484d3
unknown <Thang24122k@gmail.com>
2021-05-10 23:40:03 +0700

    convert data json to value

--- readFileJsonPHP/index.php
+++ readFileJsonPHP/index.php
@@ -1,3 +1,6 @@
 <?php
 $jsonData = file_get_contents('json\data.json');
-var_dump(json_decode($jsonData),true);
+$arrayData = array(json_decode($jsonData), true);
+foreach ($arrayData[0] as $key => $value) {
+    var_dump($value->id);
+}
