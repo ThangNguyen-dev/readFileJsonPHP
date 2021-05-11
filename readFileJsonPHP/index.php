bda5216d2a5dfb5062735aebc099d945fc84d8b3
unknown <Thang24122k@gmail.com>
2021-05-11 09:17:17 +0700

    read data done to show

--- readFileJsonPHP/index.php
+++ readFileJsonPHP/index.php
@@ -1,12 +1,5 @@
 <?php
 
-
-$jsonData = file_get_contents('json\data.json');
-$arrayData = array(json_decode($jsonData), true);
-foreach ($arrayData[0] as $key => $value) {
-    // var_dump($value->id);
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- readFileJsonPHP/layout/main.php
+++ readFileJsonPHP/layout/main.php
@@ -1,5 +1,23 @@
 <?php
-require_once '../modules/paginator.php';
+
+$jsonData = file_get_contents(__DIR__ . '\..\json\data.json');
+
+$arrayData = array(json_decode($jsonData), true);
+foreach ($arrayData[0] as $key => $value) {
+    // var_dump($value->id);
+    echo 'Name: ' . $value->name . '<br/>';
+    echo 'Gender: ' . $value->gender . '<br/>';
+    echo 'Age: ' . $value->age . '<br/>';
+    echo 'Company: ' . $value->company . '<br/>';
+    echo 'Email: ' . $value->email . '<br/>';
+    echo 'Phone: ' . $value->phone . '<br/>';
+    echo 'Address: ' . $value->address . '<br/>';
+    echo '<hr/>';
+    if ($key == 5) {
+        exit;
+    }
+}
+
 // var_dump($_SERVER['PHP_SELF']);
 // var_dump($_GET['page']);
 
@@ -11,7 +29,7 @@ $end_html = "   </ul>
             </nav>";
 $items_html = "<li class='page-item {$activELement}'>
                     <a class='rounded-circle fix-w-38 fix-h-38 text-center page-link' 
-                        href='{$_SERVER['PHP_SELF']}?page={$_GET['page']}' 
+                        href='{$_SERVER['PHP_SELF']}?page={}' 
                         >1
                     </a>
                 </li>";
