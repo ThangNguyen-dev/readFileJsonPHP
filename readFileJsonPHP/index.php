66fc6f67c3b9f0de982c0db2cb0601ca10725fc7
unknown <Thang24122k@gmail.com>
2021-05-11 07:54:35 +0700

    set layout

--- readFileJsonPHP/index.php
+++ readFileJsonPHP/index.php
@@ -1,6 +1,24 @@
 <?php
+
+
 $jsonData = file_get_contents('json\data.json');
 $arrayData = array(json_decode($jsonData), true);
 foreach ($arrayData[0] as $key => $value) {
     var_dump($value->id);
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once 'layout/header.php' ?>
+</head>
+
+<body>
+    <?php require_once 'layout/main.php'; ?>
+
+    <?php require_once 'layout/footer.php' ?>
+</body>
+
+</html>
---
+++ readFileJsonPHP/layout/header.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Task 2</title>
+<link rel="stylesheet" href="public/css/bootstrap-grid.css">
+<link rel="stylesheet" href="public/css/bootstrap.css">
+<link rel="stylesheet" href="public/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="public/css/bootstrap.css">
