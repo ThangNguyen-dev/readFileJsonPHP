bf619b7925d1da7c8b41cf91477f15a52f81f9b4
unknown <Thang24122k@gmail.com>
2021-05-11 08:24:11 +0700

    set layout pag Pagination

--- readFileJsonPHP/index.php
+++ readFileJsonPHP/index.php
@@ -4,7 +4,7 @@
 $jsonData = file_get_contents('json\data.json');
 $arrayData = array(json_decode($jsonData), true);
 foreach ($arrayData[0] as $key => $value) {
-    var_dump($value->id);
+    // var_dump($value->id);
 }
 
 ?>
--- readFileJsonPHP/layout/main.php
+++ readFileJsonPHP/layout/main.php
@@ -1,15 +1,16 @@
-<nav aria-label="...">
-    <ul class="pagination">
-        <li class="page-item">
-            <a class="page-link" href="#" tabindex="-1">Previous</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item active">
-            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-            <a class="page-link" href="#">Next</a>
-        </li>
-    </ul>
-</nav>
+<?php
+var_dump($_SERVER['PHP_SELF']);
+var_dump($_GET['page']);
+
+$activELement = 'active';
+$open_html = "<nav aria-label='...'>
+                <ul class='pagination'>";
+$end_html = "</ul>
+                </nav>";
+$items_html = "<li class='page-item {$activELement}'>
+                    <a class='page-link' 
+                        href='{$_SERVER['PHP_SELF']}?page={$_GET['page']}' 
+                        >Previous
+                    </a>
+                </li>";
+echo $open_html . $items_html . $end_html;
